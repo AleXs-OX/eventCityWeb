460c15a9df75ba5634b6bd3a6288f3b4863d8c8d
AleXs-OX <agonzm32@estudiantes.unileon.es>
2024-05-18 11:14:55 +0200

    Ready para el merge

---
+++ SW2_EventCity/src/main/java/Controller/suscriptor/suscriptorController.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller.suscriptor;
+
+/**
+ *
+ * @author AleXs
+ */
+/*Hara falta dos metodos uno que guarde y devuelva una lista con todos los eventos que hay y otro metodo
+* con todos los eventos a los que esta suscrito el usuario
+*/
+public class suscriptorController {
+    
+}
+
+
+
