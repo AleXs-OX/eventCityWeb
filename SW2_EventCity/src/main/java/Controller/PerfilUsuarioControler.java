40570b05bdd6b06dd361e8d4a227cb38dfa0c44b
Pablo González Santamarta <pablogonzalezsantamarta@MacBook-Pro-de-Pablo-4.local>
2024-05-30 19:12:11 +0200

    Cambios en PerfilUsuarioController. El controlador dará acceso a los datos del usuario comunes a todos los usuarios y además los datos de la especialización del usuario (admin, suscriptor o publicador)

--- SW2_EventCity/src/main/java/Controller/PerfilUsuarioControler.java
+++ SW2_EventCity/src/main/java/Controller/PerfilUsuarioControler.java
@@ -5,8 +5,15 @@
  */
 package Controller;
 
+import java.util.Date;
 import modelo.Usuario;
+import modelo.Admin;
+import modelo.Publicador;
+import modelo.Suscriptor;
 import EJB.UsuarioFacadeLocal;
+import EJB.SuscriptorFacadeLocal;
+import EJB.PublicadorFacadeLocal;
+import EJB.AdminFacadeLocal;
 import java.io.Serializable;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
@@ -23,15 +30,41 @@ public class PerfilUsuarioControler implements Serializable{
 
     @EJB
     private UsuarioFacadeLocal usuarioEJB;
-    
-    
+    @EJB
+    private SuscriptorFacadeLocal suscriptorEJB;
+    @EJB
+    private PublicadorFacadeLocal publicadorEJB;
+    @EJB
+    private AdminFacadeLocal adminEJB;
+
     private int idUser = 1;
     
     private Usuario usuario;
-    
+    private Publicador publicador;
+    private Suscriptor suscriptor;
+    private Admin admin;    
+
+    /**
+     * Indica el tipo de usuario para control de errores
+     */
+    private int userType = 0;
+
     @PostConstruct
     public void init(){
+	ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();
         usuario = usuarioEJB.find(idUser);
+        if (usuario == NULL){
+		System.out.println("El usuario indicado no existe. Se procede a redirigir al login");
+		ec.redirect(ec.getRequestContextPath() + "/loginUsuario.xhtml");
+	}
+	else{
+		suscriptor = suscriptorEJB.find(idUser);
+		publicador = publicadorEJB.find(idUser);
+		admin = adminEJB.find(idUser);
+		if (suscriptor != NULL) userType = 1;
+		elseif (publicador != NULL) userType = 2;
+		elseif (admin != NULL) userType = 3;
+	}
     }
 
     /**
@@ -39,9 +72,22 @@ public class PerfilUsuarioControler implements Serializable{
      */
     public boolean saveChanges(){
         usuarioEJB.edit(usuario);
+	switch (tipoUsuario){
+		case 1:
+			suscriptorEJB.edit(suscriptor);
+			break;
+		case 2:
+			publicadorEJB.edit(publicador);
+			break;
+		case 3:
+			adminEJB.edit(admin);
+			break;
+	}
         return true;
     }
     
+    // Getters y Setters comunes a todos los usuarios
+
     public void setUsername(String username){
         usuario.setNombreusuario(username);
     }
@@ -81,5 +127,68 @@ public class PerfilUsuarioControler implements Serializable{
     public String getEmail(){
         return usuario.getEmail();
     }
-    
+
+    // Getters y Setters exclusivos de suscriptores
+
+    public int getNumSuscripciones(){
+	if (userType == 1) return suscriptor.getNumSuscripciones();
+        return 0;
+    }
+
+    public String getLocation(){
+	if (userType == 1) return suscriptor.getDireccion() + ", " + suscriptor.getCiudad() + ", " + suscriptor.getPais();
+        return "";
+    }
+
+    public void setDireccion(String direccion){
+	if (userType == 1) suscriptor.setDireccion(direccion);
+    }
+
+    public void setCiudad(String ciudad){
+	if (userType == 1) suscriptor.setCiudad(ciudad);
+    }
+
+    public void setPais(String pais){
+	if (userType == 1) suscriptor.setPais(pais);
+    }
+
+    // Getters y setters exclusivos de publicadores
+
+    public Date getFechaAlta(){
+	if (userType == 2) return publicador.getFechaAlta();
+        return NULL;
+    }
+
+    public int getNumEventos(){
+	if (userType == 2) return publicador.getNumEventos();
+        return 0; 
+    }
+   
+    public String getDescripcion(){
+	if (userType == 2) return publicador.getDescripcion();
+        return "";
+    }   
+     
+    public void setFechaAlta(Date fecha){
+	if (userType == 2) publicador.setFechaAlta(fecha);
+    }
+
+    public void setNumEventos(int eventos){
+	if (userType == 2) publicador.setNumEventos(eventos);
+    }
+   
+    public void setDescripcion(String descripcion){
+	if (userType == 2) return publicador.setDescripcion(String descripcion);
+    }   
+   
+    // Getters y setters exclusivos de administradores
+
+    public String getRol(){
+	if (userType == 3) return admin.getRol();
+	return "";
+    }
+
+    public void setRol(String rol){
+	if (userType == 3) admin.setRol(rol); 
+    }
 }
